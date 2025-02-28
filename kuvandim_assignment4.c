9cf0d9220a450064719558fda4d5d15520ddaf85
msk.linux <skuvandik@gmail.com>
2025-02-28 11:49:01 +0300

    Add execute_input function to handle the three built-in commands

--- kuvandim_assignment4.c
+++ kuvandim_assignment4.c
@@ -14,6 +14,15 @@
 // type definitions
 
 // external variables
+int last_fg_status = 0;
+int last_fg_signal = 0;
+bool last_fg_terminated_by_signal = false;
+
+/*
+ * The following struct is taken from the file "sample_parser.c" provided in:
+ * Operating Systems I (CS 374, Winter 2025), Oregon State University.
+ * Course Instructors: Chaudhry, N., Tonsmann, G.
+ */
 struct command_line {
         char *argv[MAX_ARGS + 1];
         int argc;
@@ -24,6 +33,7 @@ struct command_line {
 
 // prototypes
 struct command_line *parse_input(void);
+void execute_input(struct command_line *command);
 
 // main
 int main()
@@ -32,6 +42,7 @@ int main()
 
         while (true) {
                 curr_command = parse_input();
+                execute_input(curr_command);
         }
 
         return EXIT_SUCCESS;
@@ -40,8 +51,8 @@ int main()
 // other functions
 
 /*
- * Following function is taken from the file "sample_parser.c" provided in:
- * "Operating Systems I (CS 374, Winter 2025)" Oregon State University.
+ * The following function is taken from the file "sample_parser.c" provided in:
+ * Operating Systems I (CS 374, Winter 2025), Oregon State University.
  * Course Instructors: Chaudhry, N., Tonsmann, G.
  */
 struct command_line *parse_input(void)
@@ -70,4 +81,54 @@ struct command_line *parse_input(void)
         }
 
         return curr_command;
+}
+
+void execute_input(struct command_line *command)
+{
+        if (command->argc == 0) {
+                free(command);
+                return;
+        }
+
+        if (command->argv[0][0] == '#') {
+                free(command);
+                return;
+        }
+
+        if ((strcmp(command->argv[0], "exit")) == 0) {
+                // Background processes?
+                free(command);
+                exit(EXIT_SUCCESS);
+        }
+
+        if ((strcmp(command->argv[0], "cd")) == 0) {
+
+                if (command->argc == 1) {
+                        char *home = getenv("HOME");
+                        chdir(home);
+                }
+
+                if (command->argc == 2) {
+                        chdir(command->argv[1]);
+                }
+
+                free(command);
+                return;
+        }
+
+        if ((strcmp(command->argv[0], "status")) == 0) {
+
+                if (last_fg_terminated_by_signal) {
+                        printf("terminated by signal %d\n", last_fg_signal);
+                } else {
+                        printf("exit value %d\n", last_fg_status);
+                }
+
+                fflush(stdout);
+                free(command);
+                return;
+        }
+
+        // Non built-in commands go here!
+        free(command);
 }
